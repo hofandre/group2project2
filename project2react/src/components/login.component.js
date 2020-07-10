6991a6401d5dfb74fd4622d454f9f43ccb4835f2
[heredia-jlg] <[heredia.jlg@gmail.com]>
2020-07-10 00:24:59 -0500

    Comments can be deleted. Visually improved  navbar, comments, and login component

--- project2react/src/components/login.component.js
+++ project2react/src/components/login.component.js
@@ -102,7 +102,7 @@ class Login extends Component {
         console.log(this.props.accuracy)
         return (
             <>
-                <Nav bsStyle="default" style={{width: "550px"}}>
+                <Nav bsstyle="default" style={{width: "550px"}}>
                     <Col>
                         <Form />
                             <Navbar.Text >
