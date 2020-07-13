85fb1dd3181fcc51d2934b99aeed17744db95135
jcarevature <Justin.Adams@uvm.edu>
2020-07-12 20:58:13 -0400

    Testing worked in all cases except pendingSet.

--- project2react/src/App.test.js
+++ project2react/src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount, shallow, render } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const initialState = {
+      file_one: null,
+      file_two: null,
+      keywords: [],
+      file: '',
+      title: '',
+      file_name_one: '',
+      file_name_two: '',
+      alt_text_one: '',
+      alt_text_two: '',
+      user: {username:'', password:'', usertype:''},
+      username: '',
+      registerUser: '',
+      password: '',
+      registerPassword: '',
+      registerAge: 0,
+      minStrength: 3,
+      minLength: 7,
+      confirm: '',
+      role: '',
+      aggregateStats: {},
+      comment: '',
+      register: {username: '', password: '', usertype: ''},
+      displaySets: {},
+      displayPendingSets: {},
+      displayComments: {},
+      displayUsers: {},
+      correct_option: 0,
+      accuracy: 0.0,
+      displaySetCriteria: 0,
+      displaySearchTerm: 'id',
+      displaySetAccuracy: {},
+      lastSearchMade: {type: '', param: ''},
+      deckSets: []
+  };
+  const mockStore = configureStore();
+  let store,container
+
+  beforeEach(()=>{
+      store = mockStore(initialState);
+      container = shallow(<App store={store} /> )
+      it('+++ render the connected(SMART) component', () => {
+         expect(container.length).toEqual(1);
+      });
+  });
 });
--- project2react/src/components/passwordfield.test.js
+++ project2react/src/components/passwordfield.test.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import PasswordField from './passwordfield.component.js';
 describe('PasswordField', () => {
-  it('should render correctly in "debug" mode', () => {    const component = shallow(<PasswordField debug />);
+  const initialState = {
+      accuracy: 0.0,
+      aggregateStats: {},
+      alt_text_one: '',
+      alt_text_two: '',
+      comment: '',
+      confirm: '',
+      correct_option: 0,
+      deckSets: [],
+      displayComments: {},
+      displayPendingSets: {},
+      displaySets: {},
+      displaySearchTerm: 'id',
+      displaySetCriteria: 0,
+      displaySetAccuracy: {},
+      displayUsers: {},
+      file: '',
+      file_one: null,
+      file_two: null,
+      file_name_one: '',
+      file_name_two: '',
+      keywords: [],
+      lastSearchMade: {type: '', param: ''},
+      minStrength: 3,
+      minLength: 7,
+      password: '',
+      pendingSet: {title: ''},
+      register: {username: '', password: '', usertype: ''},
+      registerAge: 0,
+      registerPassword: '',
+      registerUser: '',
+      role: '',
+      set: {title: ''},
+      title: '',
+      user: {username:'', password:'', usertype:''},
+      username: ''
+  };
+  const mockStore = configureStore();
+  let store,container
 
-    expect(component).toMatchSnapshot();
+  beforeEach(()=>{
+      store = mockStore(initialState);
+      container = shallow(<PasswordField store={store} /> )
+  })
+
+  it('+++ render the connected(SMART) component', () => {
+     expect(container.length).toEqual(1);
+  });
+
+  it('+++ check Prop matches with initialState', () => {
+     expect(container.prop('output')).toEqual(initialState.output);
   });
 });
